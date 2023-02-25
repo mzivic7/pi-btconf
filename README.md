338ba5f31224314ebb56c086b1fd6835763d089e
mzivic7 <marko.b.zivic@gmail.com>
2023-02-25 17:50:50 +0100

    Hold button to shutdown
    Added button when_released and moved main code to released() function

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ To install, run following commands:
 git clone https://github.com/mzivic7/py-btconf
 sudo pi-btconf/install.sh
 ```
-This will install all dependancies and configure service that waits for button double-press.
+This will install all dependancies and configure service that waits for button input.
 
 ## Usage
 Pair raspberry pi with second device.
-Connect button to GPIO pin 3 and gnd.
-To start pi-btconf, push button twice under 0.5 seconds. This will also terminate pi-btconf if it is previously started and turn off bluetooth.
+Connect button to GPIO pin 3 and gnd (can be changed).
+To start pi-btconf, push button twice under 0.5 seconds. This will also terminate pi-btconf if it is active and turn off bluetooth.
 Pi-btconf will turn on bluetooth if it is off.
 
 On second device any terminal that communicates over bluetooth RFCOMM protocol will work.
@@ -20,6 +20,7 @@ There are many apps designed for android bluetooth RFCOMM (like [BlueTerm](https
 
 ## Features:
  - Swich on / off script and bluetooth on button double-press
+ - Hold button for 5s to shutdown device
  - Setup terminal over bluetooth RFCOMM
  - List saved networks
  - Scan for available networks
@@ -35,6 +36,13 @@ There are many apps designed for android bluetooth RFCOMM (like [BlueTerm](https
  - Run pre-set custom command in terminal
  - End session and disable bluetooth
 
+## Config
+Some launcher settings can be configured inside script.  
+Launcher script can be found in: usr/local/sbin/pi-btconf-launcher.py  
+Following variables can be modified:
+- `gpio_btn = 3`   - launch button gpio pin
+- `hold_s = 5`   - seconds to hold button to shutdown device
+
 ## Terminal:
 
 Terminal function will work with any RFCOMM terminal on second device. 
@@ -45,6 +53,6 @@ Pressing button twice will turn off bluetooth and disconnect terminal.
 ## Custom Commands:
 Custom commands can be configured in: /etc/pi-btconf/custom_comands.txt  
 Format:  
-name = "command to run in terminal"  
+`name = "command to run in terminal"`  
 Each line represents one command.  
 There must be " = " separator and command must be inside "".
--- pi-btconf/pi-btconf-launcher.py
+++ pi-btconf/pi-btconf-launcher.py
@@ -3,38 +3,48 @@ from signal import pause
 from datetime import datetime, timedelta
 import subprocess
 
-gpio_btn = 3   # launch button
+gpio_btn = 3   # launch button gpio pin
+hold_s = 5   # seconds to hold button to shutdown device
 
 Button.pressed_time = None   # when the button is pressed
+Button.released_time = None   # when the button is released
 Button.running = False   # is pi-btconf script running
 Button.script = None   # scrpit subprocess object
 
 subprocess.Popen("rfkill block bluetooth", shell=True)  # disable bluetooth at boot
 
 def pressed(btn):
-    if btn.pressed_time:   # if button is already pressed once
-        if btn.pressed_time + timedelta(seconds=0.5) > datetime.now():   # if new time is 0.5s greater than previous
-            if btn.pressed_time + timedelta(seconds=0.05) < datetime.now():   # if button is not too fast pushed
-                btn.running = not btn.running   # invert script running state
-                if btn.running is True:   # if script is to be running:
-                    btn.script = subprocess.Popen(['/usr/bin/python3', '/usr/local/sbin/pi-btconf.py'])   # run script
-                else:   # if script is to be terminated:
-                    if btn.script.poll() is None:   # if script is still running:
-                        btn.script.terminate()   # terminate script and disable bluetooth
-                        subprocess.Popen("rfkill block bluetooth", shell=True)
-                    else:   # if script has already been terminated
-                        btn.script = subprocess.Popen(['/usr/bin/python3', '/usr/local/sbin/pi-btconf.py'])   # run script again
-                        btn.running = True   # set script state to running
-                btn.pressed_time = None   # save button pressed time
-            else:    # if button is too fast pushed:
-                btn.pressed_time = datetime.now()   # save button pressed time
-        else:   # if to much time has passed: 
-            btn.pressed_time = datetime.now()   # save button pressed time
-    else:   # if button is not pressed
-        btn.pressed_time = datetime.now()   # save button pressed time
+    btn.pressed_time = datetime.now()   # save button pressed time
+
+def released(btn):
+    if btn.pressed_time + timedelta(seconds=hold_s) > datetime.now():   # if button is released after less than n seconds
+        if btn.released_time:   # if button is already pressed once
+            if btn.released_time + timedelta(seconds=0.5) > datetime.now():   # if new time is 0.5s greater than previous
+                if btn.released_time + timedelta(seconds=0.05) < datetime.now():   # if button is not too fast pressed
+                    btn.running = not btn.running   # invert script running state
+                    if btn.running is True:   # if script is to be running:
+                        btn.script = subprocess.Popen(['/usr/bin/python3', '/usr/local/sbin/pi-btconf.py'])   # run script
+                    else:   # if script is to be terminated:
+                        if btn.script.poll() is None:   # if script is still running:
+                            btn.script.terminate()   # terminate script and disable bluetooth
+                            subprocess.Popen("rfkill block bluetooth", shell=True)
+                        else:   # if script has already been terminated
+                            btn.script = subprocess.Popen(['/usr/bin/python3', '/usr/local/sbin/pi-btconf.py'])   # run script again
+                            btn.running = True   # set script state to running
+                    btn.released_time = None   # save button released time
+                else:    # if button is too fast pushed:
+                    btn.released_time = datetime.now()   # save button released time
+            else:   # if to much time has passed: 
+                btn.released_time = datetime.now()   # save button released time
+        else:   # if button is not pressed before
+            btn.released_time = datetime.now()   # save button released time
+    else:   # if button is released afte more than 6 seconds
+        subprocess.Popen("shutdown now", shell=True)   # shutdown device
 
 
 btn = Button(gpio_btn)   # define button object
 btn.when_pressed = pressed   # when button is pressed activate function
+btn.when_released = released   # when button is pressed activate function
 
 pause()
+
